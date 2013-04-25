ee8ec7b3abbb6c4b1b12b255610387c8faa57e53
tpetty <tpetty@synacor.com>
2013-04-25 12:51:18 -0400

    added a plugin

--- wp-content/themes/twentytwelve/page-templates/front-page.php
+++ wp-content/themes/twentytwelve/page-templates/front-page.php
@@ -16,17 +16,9 @@ get_header(); ?>
 
 	<div id="primary" class="site-content">
 		<div id="content" role="main">
-
-			<?php while ( have_posts() ) : the_post(); ?>
-				<?php if ( has_post_thumbnail() ) : ?>
-					<div class="entry-page-image">
-						<?php the_post_thumbnail(); ?>
-					</div><!-- .entry-page-image -->
-				<?php endif; ?>
-
-				<?php get_template_part( 'content', 'page' ); ?>
-
-			<?php endwhile; // end of the loop. ?>
+		<?php wp_list_pages(array(
+			'depth' => 1,
+		)); ?>
 
 		</div><!-- #content -->
 	</div><!-- #primary -->
