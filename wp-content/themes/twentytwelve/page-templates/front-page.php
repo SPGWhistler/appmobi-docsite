a58f970856ac79cf8328b3d334de2c0a379acfb8
Tony Petty <tpetty@appmobi.com>
2013-04-29 11:42:42 -0400

    Added styles and html to do boxes.

--- wp-content/themes/twentytwelve/page-templates/front-page.php
+++ wp-content/themes/twentytwelve/page-templates/front-page.php
@@ -11,25 +11,124 @@
  * @subpackage Twenty_Twelve
  * @since Twenty Twelve 1.0
  */
+?>
 
-get_header(); ?>
+<style>
+/*
+  First define a content are width and position, if needed. I'm centering
+  the 623px wide shell
+*/
+#clb-shell {
+  margin : auto;
+  width : 825px;
+}
 
-	<div id="primary" class="site-content">
-		<div id="content" role="main">
-			<?php
-			$id = get_the_ID();
-			$args = array(
-				'parent' => 0
-			);
-			$pages = get_pages($args);
-			foreach ($pages as $page)
-			{
-				echo "<a href='" . $page->guid . "'>" . $page->post_title . "</a>";
-				echo "<span>" . get_metadata('post', $page->ID, 'appmobi_page_description', true) . "</span><br>";
-			}
-			?>
-		</div><!-- #content -->
-	</div><!-- #primary -->
+/*
+  Now I'm resetting the paragraph padding (it was zeroed). I'm also clearing
+  the p, but I don't really need this for the top one, only the bottom. The 
+  top has no effect (it'll clear the heading by default), but it is needed 
+  for the bottom (depending on the shell width versus the ul width). It may 
+  not be needed at all. Your situation will determine.
+*/
+#clb-shell p {
+  padding : 10px;
+  clear : both;
+}
 
+/*
+  I define the width of the ul, set the height (in ems!) align the text and
+  remove list styles. The last may not be needed on most browsers
+*/
+ul#clb {
+  width : 825px;
+  height : 9em;
+  text-align : center;
+  list-style-type : none;
+}
+
+/*
+  Now I style the individual boxes (li)
+*/
+ul#clb li {
+  margin : 0 2px;
+  border : 1px solid #666;
+  width : 200px;
+  height : auto;
+  /*background : #ffffea url(images/clb_li_back.jpg);*/
+  background : #ffffea;
+  float : left;
+  display : inline;
+}
+
+/*
+  Style the li links
+*/
+#clb-shell a {
+  color : #669900;
+}
+#clb-shell a:hover, #clb-shell a:focus, #clb-shell a:active {
+  color : #000;
+  text-decoration : none;
+}
+#clb-shell a:focus, #clb-shell a:active {
+  background-color : #fff;
+}
+
+/*
+  Style the h3 links
+*/
+#clb-shell ul#clb h3 a {
+  color : #ffffaa;
+  display : block;
+  width : 194px;
+  padding : 2px 3px;
+  /*background : #333 url(images/clb_h3_back.jpg) repeat-x;*/
+  background : #333;
+  border-bottom : 1px solid #666;
+  text-decoration : none;
+}
+#clb-shell ul#clb h3 a:hover, #clb-shell ul#clb h3 a:focus, #clb-shell ul#clb h3 a:active {
+  /*background : #957412 url(images/clb_h3_back_over.jpg) repeat-x;*/
+  background : #957412;
+  color : #fff;
+}
+
+/*
+  This tyles the text p content within the li separately. The most important 
+  thing here is to re-kill the padding and add the margin to create good gutters
+*/
+ul#clb p {
+  font-size : 0.9em;
+  padding : 0;
+  margin : 10px;
+}
+</style>
+
+<?php get_header(); ?>
+<div id="primary" class="site-content">
+	<div id="content" role="main">
+		<?php
+		$args = array(
+			'parent' => 0,
+			'sort_column' => 'menu_order',
+		);
+		$pages = get_pages($args);
+		?>
+		<div id="clb-shell">
+			<ul id="clb">
+				<?php
+				foreach ($pages as $page)
+				{
+					if ($page->post_title !== 'home')
+					{
+						echo "<li><h3><a href='" . $page->guid . "'>" . $page->post_title . "</a></h3>";
+						echo "<p>" . get_metadata('post', $page->ID, 'appmobi_page_description', true) . "</p></li>";
+					}
+				}
+				?>
+			</ul>
+		</div>
+	</div><!-- #content -->
+</div><!-- #primary -->
 <?php get_sidebar( 'front' ); ?>
 <?php get_footer(); ?>
